4e75722e83bc24420bbe00013ee3baae363aa04d
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-09-26 21:23:38 -0700

    refactor UI module as class with static methods

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import { TodoList } from "./modules/TodoList";
 import { Task } from "./modules/Task";
 import "./modules/UI";
 import "./style.css";
+import { UI } from "./modules/UI";
+
+UI.initializeApp();
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,113 +1,126 @@
 import { Task } from "./Task";
-import { TodoList } from "./TodoList";
 import { Storage } from "./Storage";
 
 const todoListArray = Storage.parseLocalStorage();
-const addNewTaskBtn = document.getElementById("add-new-task-btn");
-const todoInput = document.getElementById("new-task-input");
-const todoList = document.getElementById("todo-list");
 
-const createTodoItem = (newTaskName, isDone) => {
-  const todoItem = document.createElement("div");
-  const todoName = document.createElement("div");
-  const todoActions = document.createElement("div");
-  todoItem.classList.add("todo-item");
-  todoName.classList.add("todo-name");
-  todoActions.classList.add("todo-actions");
+export class UI {
+  static addNewTask = () => {
+    const todoInput = document.getElementById("new-task-input");
+    const newTask = new Task(todoInput.value, false);
 
-  const label = document.createElement("label");
-  const checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("id", "checkbox");
-  if (isDone) {
-    checkbox.checked = true;
-    todoItem.classList.add("done");
-  }
-  label.appendChild(checkbox);
-
-  const todoNameInput = document.createElement("input");
-  todoNameInput.setAttribute("type", "text");
-  todoNameInput.setAttribute("value", `${newTaskName}`);
-  todoNameInput.setAttribute("readonly", true);
-  todoName.appendChild(todoNameInput);
-
-  const editBtn = document.createElement("button");
-  const deleteBtn = document.createElement("button");
-  const editBtnIcon = document.createElement("i");
-  const deleteBtnIcon = document.createElement("i");
-  editBtn.classList.add("edit");
-  deleteBtn.classList.add("delete");
-  editBtnIcon.classList.add("fa-solid", "fa-pen");
-  deleteBtnIcon.classList.add("fa-solid", "fa-trash");
-  editBtn.appendChild(editBtnIcon);
-  deleteBtn.appendChild(deleteBtnIcon);
-  todoActions.appendChild(editBtn);
-  todoActions.appendChild(deleteBtn);
-
-  checkbox.addEventListener("change", (e) => {
-    if (e.target.checked) {
-      todoItem.classList.add("done");
-      todoListArray.getTask(newTaskName).toggleDone();
-      Storage.saveTodoList(todoListArray);
-    } else {
-      todoItem.classList.remove("done");
-      todoListArray.getTask(newTaskName).toggleDone();
-      Storage.saveTodoList(todoListArray);
+    if (todoInput.value == "") {
+      alert("Please input a task");
+      return false;
     }
-    displayTodoList();
-  });
 
-  editBtn.addEventListener("click", () => {
-    todoNameInput.removeAttribute("readonly");
-    todoNameInput.focus();
-    todoNameInput.select();
-    todoNameInput.addEventListener("blur", () => {
-      todoNameInput.setAttribute("readonly", true);
-      todoListArray.getTask(newTaskName).name = todoNameInput.value;
-      Storage.saveTodoList(todoListArray);
-      displayTodoList();
-    });
-  });
-
-  deleteBtn.addEventListener("click", () => {
-    todoListArray.removeTask(newTaskName);
+    todoListArray.addTask(newTask);
     Storage.saveTodoList(todoListArray);
-    displayTodoList();
-  });
+    todoInput.value = "";
+    UI.displayTodoList();
+  };
 
-  todoItem.appendChild(label);
-  todoItem.appendChild(todoName);
-  todoItem.appendChild(todoActions);
-  return todoItem;
-};
+  static displayTodoList = () => {
+    const todoList = document.getElementById("todo-list");
 
-const addNewTask = () => {
-  if (todoInput.value == "") {
-    alert("Please input a task");
-    return false;
-  }
-  const newTask = new Task(todoInput.value, false);
-  todoListArray.addTask(newTask);
-  Storage.saveTodoList(todoListArray);
-  displayTodoList();
+    todoList.innerHTML = "";
+    todoListArray.tasks.forEach((todo) => {
+      todoList.appendChild(UI.createTodoItem(todo.name, todo.isDone));
+    });
+  };
 
-  todoInput.value = "";
-};
+  static createTodoItem = (newTaskName, isDone) => {
+    const todoItem = document.createElement("div");
+    const todoName = document.createElement("div");
+    const todoActions = document.createElement("div");
+    todoItem.classList.add("todo-item");
+    todoName.classList.add("todo-name");
+    todoActions.classList.add("todo-actions");
 
-const displayTodoList = () => {
-  todoList.innerHTML = "";
-  todoListArray.tasks.forEach((todo) => {
-    todoList.appendChild(createTodoItem(todo.name, todo.isDone));
-  });
-};
+    /* Create checkbox element */
 
-addNewTaskBtn.addEventListener("click", addNewTask);
-todoInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    addNewTask();
-  }
-});
+    const label = document.createElement("label");
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", "checkbox");
+    if (isDone) {
+      checkbox.checked = true;
+      todoItem.classList.add("done");
+    }
+    label.appendChild(checkbox);
 
-window.onload = () => {
-  displayTodoList();
-};
+    /* Create task name element */
+
+    const todoNameInput = document.createElement("input");
+    todoNameInput.setAttribute("type", "text");
+    todoNameInput.setAttribute("value", `${newTaskName}`);
+    todoNameInput.setAttribute("readonly", true);
+    todoName.appendChild(todoNameInput);
+
+    /* Create edit and delete buttons */
+
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+    const editBtnIcon = document.createElement("i");
+    const deleteBtnIcon = document.createElement("i");
+    editBtn.classList.add("edit");
+    deleteBtn.classList.add("delete");
+    editBtnIcon.classList.add("fa-solid", "fa-pen");
+    deleteBtnIcon.classList.add("fa-solid", "fa-trash");
+    editBtn.appendChild(editBtnIcon);
+    deleteBtn.appendChild(deleteBtnIcon);
+    todoActions.appendChild(editBtn);
+    todoActions.appendChild(deleteBtn);
+
+    checkbox.addEventListener("change", (e) => {
+      if (e.target.checked) {
+        todoItem.classList.add("done");
+        todoListArray.getTask(newTaskName).toggleDone();
+        Storage.saveTodoList(todoListArray);
+      } else {
+        todoItem.classList.remove("done");
+        todoListArray.getTask(newTaskName).toggleDone();
+        Storage.saveTodoList(todoListArray);
+      }
+      UI.displayTodoList();
+    });
+
+    editBtn.addEventListener("click", () => {
+      todoNameInput.removeAttribute("readonly");
+      todoNameInput.focus();
+      todoNameInput.select();
+      todoNameInput.addEventListener("blur", () => {
+        todoNameInput.setAttribute("readonly", true);
+        todoListArray.getTask(newTaskName).name = todoNameInput.value;
+        Storage.saveTodoList(todoListArray);
+        UI.displayTodoList();
+      });
+    });
+
+    deleteBtn.addEventListener("click", () => {
+      todoListArray.removeTask(newTaskName);
+      Storage.saveTodoList(todoListArray);
+      UI.displayTodoList();
+    });
+
+    todoItem.appendChild(label);
+    todoItem.appendChild(todoName);
+    todoItem.appendChild(todoActions);
+    return todoItem;
+  };
+
+  static initializeApp = () => {
+    const addNewTaskBtn = document.getElementById("add-new-task-btn");
+    const todoInput = document.getElementById("new-task-input");
+
+    addNewTaskBtn.addEventListener("click", UI.addNewTask);
+    todoInput.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        UI.addNewTask();
+      }
+    });
+
+    window.onload = () => {
+      UI.displayTodoList();
+    };
+  };
+}
